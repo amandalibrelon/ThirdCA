8b235c85361e155fef6b4a9dd3a31420201de422
amandalibrelon <152657405+amandalibrelon@users.noreply.github.com>
2024-03-24 13:32:18 +0000

    this class is responsible for establishing a connection to the MySQL database
    - using JDBC
    - it contains methods for connecting to the database, executing queries, and handling exceptions.

--- src/thirdca/DBConnector.java
+++ src/thirdca/DBConnector.java
@@ -4,10 +4,36 @@
  */
 package thirdca;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  *
  * @author amand
  */
 public class DBConnector {
+    private static final String JDBC_URL = "jdbc:mysql://localhost:3306/cms";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "Trindade-91";
+    
+    public static Connection getConnection() throws SQLException {
+        Connection connection = null;
+        
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection(JDBC_URL, USERNAME, PASSWORD);
+        } catch (ClassNotFoundException e) {
+            System.out.println("MySQL JDBC driver not found.");
+            e.printStackTrace();
+            //this logs exception or display a user-friendly error message
+        } catch (SQLException e) {
+            System.out.println("Failed to connect with database");
+            e.printStackTrace();
+        }
+        
+        return connection;
+    }
+    
     
 }
