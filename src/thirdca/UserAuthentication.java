82e1f56025fa623cc7615069a9863a165dfc8abb
amandalibrelon <152657405+amandalibrelon@users.noreply.github.com>
2024-03-24 21:13:27 +0000

    restructured my code in order for it to work and look better
    - there are errors coming up, java suggested to create a new user class, I'll look into that later so far I have done:
    - Method to authenticate users.
    - Method to change a user's password.
    - Method to manage users (for Admin role)
    - Class to represent a user.
    - Constructor to initialize a User object.
    - Getters
    - Change of password

--- src/thirdca/UserAuthentication.java
+++ src/thirdca/UserAuthentication.java
@@ -10,57 +10,79 @@ import java.util.Map;
 public class UserAuthentication {
 
     private Map<String, User> users;
-    
+
     public UserAuthentication() {
         //initialize users 
         users = new HashMap<>();
         users.put("admin", new User("admin", "admin123", UserRole.ADMIN));
+        users.put("office", new User("office", "office123", UserRole.OFFICE));
+        users.put("lecturer", new User("lecturer", "lecturer123", UserRole.LECTURER));
     }
-    
-    
-    
-    
-    
-    private final String username;
-    private final String password;
-    private final UserRole role;
-   
-    
-    class User {
-        private String username;
-        private String password;
-        private UserRole role;
-        
+
+    //Method to authenticate users
+    public boolean autheticate(String username, String password) {
+        user user = users.get(username);
+        return users != null && user.getPassword().equals(password);
+
     }
-     public UserAuthentication(String username, String password, UserRole role) {
-            this.username = username;
-            this.password = password;
-            this.role = role;
-    }
-     
-     public String getUsername() {
-        return username;
-        
-     }    
-          
-       public String getPassword() {
-        return password;
-    }
-            
-        public UserRole getRole() {
-        return role;
-    }
-        
-        public void changePassword(String newPassword) {
-            this.password = newPassword;
+  
+
+    //method to manage users (for Admin role)
+    public void manageUser(String action, String username, String password, UserRole role) {
+        if (action.equals("add")) {
+            users.put(username, new User(username, password, role));
+        } else if (action.equals("change")) {
+            User user = users.get(username);
+            if (user != null) {
+                user.changePassword(password);
+                user.role = role;
+            }
+
+        } else if (action.equals("delete")) {
+            users.remove(username);
         }
-            
-      enum UserRole {
+    }
+
+    enum UserRole {
         ADMIN,
         OFFICE,
         LECTURER
-    }       
-            
-            
-            
+    }
+
+    //class to repredent a user
+    class User {
+
+        private String username;
+        private String password;
+        private UserRole role;
+
+        public User(String username, String password, UserRole role) {
+            this.username = username;
+            this.password = password;
+            this.role = role;
+        }
+
+    public String getUsername() {
+        return username;
+
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public UserRole getRole() {
+        return role;
+    }
+
+    public void changePassword(String newPassword) {
+        this.password = newPassword;
+    }
+    
+    //method to change password for the user
+    public void changePassword(String newPassword) {
+        this.password = newPassword;
+    }
+
+    }
 }
