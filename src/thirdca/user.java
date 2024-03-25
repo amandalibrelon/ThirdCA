b1edce49105e937a926d7f80d15edcedb54c840e
amandalibrelon <152657405+amandalibrelon@users.noreply.github.com>
2024-03-25 18:20:57 +0000

    UserAuthentication class deleted
    - as it made no sense to have it

--- src/thirdca/user.java
+++ src/thirdca/user.java
@@ -4,11 +4,14 @@
  */
 package thirdca;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  *
  * @author amand
  */
-class user {
+
     
     // Enum for user roles
 enum UserRole {
@@ -18,7 +21,7 @@ enum UserRole {
 }
 
 // Class to represent a user
-class User {
+public class User {
     private String username;
     private String password;
     private UserRole role;
@@ -47,5 +50,44 @@ class User {
         this.password = newPassword;
     }
     
+    class UserAuthentication {
+    private Map<String, User> users;
+
+    public UserAuthentication() {
+        // Initialize users (this can be loaded from a database)
+        users = new HashMap<>();
+        users.put("admin", new User("admin", "admin123", UserRole.ADMIN));
+        users.put("office", new User("office", "office123", UserRole.OFFICE));
+        users.put("lecturer", new User("lecturer", "lecturer123", UserRole.LECTURER));
+    }
+
+    // Method to authenticate users
+    public boolean authenticate(String username, String password) {
+        User user = users.get(username);
+        return user != null && user.getPassword().equals(password);
+    }
+
+    // Method to change password
+    public void changePassword(String username, String newPassword) {
+        User user = users.get(username);
+        if (user != null) {
+            user.changePassword(newPassword);
+        }
+    }
+
+    // Method to manage users (for Admin role)
+    public void manageUser(String action, String username, String password, UserRole role) {
+        if (action.equals("add")) {
+            users.put(username, new User(username, password, role));
+        } else if (action.equals("modify")) {
+            User user = users.get(username);
+            if (user != null) {
+                user.changePassword(password);
+                user.role = role;
+            }
+        } else if (action.equals("delete")) {
+            users.remove(username);
+        }
+    }
 }
 }
