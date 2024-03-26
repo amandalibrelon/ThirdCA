9752855238f600ea33089260d91b69a4e7a70269
amandalibrelon <152657405+amandalibrelon@users.noreply.github.com>
2024-03-26 19:37:33 +0000

    course, student and lecturer classes created
    course class: instanced variables, constructor to initialize class, getters and setters.
    - class encapsulates the essential properties and behaviour of same

--- src/thirdca/Course.java
+++ src/thirdca/Course.java
@@ -4,10 +4,72 @@
  */
 package thirdca;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author amand
  */
-class Course {
+public class Course { // class to represent courses
     
+    private String courseName;
+    private String program;
+    private String roomAssignment;
+    private List<Student> enrolledStudents;
+    private Lecturer lecturer;
+    
+    
+    // constructor
+    public Course(String courseName, String program, String roomAssignment, Lecturer lecturer){
+       this.courseName = courseName;
+       this.program = program; 
+       this.roomAssignment = roomAssignment; 
+       this.lecturer = lecturer;
+       this.enrolledStudents = new ArrayList<>();
+       
+    }
+    // Getters and setters
+ 
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public String getProgram() {
+        return program;
+    }
+
+    public void setProgram(String program) {
+        this.program = program;
+    }
+
+    public String getRoomAssignment() {
+        return roomAssignment;
+    }
+
+    public void setRoomAssignment(String roomAssignment) {
+        this.roomAssignment = roomAssignment;
+    }
+
+    public List<Student> getEnrolledStudents() {
+        return enrolledStudents;
+    }
+
+    public void setEnrolledStudents(List<Student> enrolledStudents) {
+        this.enrolledStudents = enrolledStudents;
+    }
+
+    public Lecturer getLecturer() {
+        return lecturer;
+    }
+
+    public void setLecturer(Lecturer lecturer) {
+        this.lecturer = lecturer;
+    }
 }
+    
