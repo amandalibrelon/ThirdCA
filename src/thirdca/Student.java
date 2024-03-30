ad47475cd629cef34290a59875a521fa11321aad
amandalibrelon <152657405+amandalibrelon@users.noreply.github.com>
2024-03-30 12:41:28 +0000

    represents a student in the course management system.
    - Constructs a new Student object with the specified details
    - Returns a string representation of the Student object.
    - Create a new Student object using the constructor
    - Print student information

--- src/thirdca/Student.java
+++ src/thirdca/Student.java
@@ -8,6 +8,72 @@ package thirdca;
  *
  * @author amand
  */
-class Student {
-    
+
+// Class to represent students
+public class Student {
+    private String studentId;
+    private String studentName;
+    private String currentModules;
+    private String repeatModules;
+    private String completedModules;
+    private String programme;
+
+    // Constructor
+    public Student(String studentId, String studentName, String currentModules, String repeatModules, String completedModules, String programme) {
+        this.studentId = studentId;
+        this.studentName = studentName;
+        this.currentModules = currentModules;
+        this.repeatModules = repeatModules;
+        this.completedModules = completedModules;
+        this.programme = programme;
+    }
+
+   // Getters and setters
+    public String getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(String studentId) {
+        this.studentId = studentId;
+    }
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+
+    public String getCurrentModules() {
+        return currentModules;
+    }
+
+    public void setCurrentModules(String currentModules) {
+        this.currentModules = currentModules;
+    }
+
+    public String getRepeatModules() {
+        return repeatModules;
+    }
+
+    public void setRepeatModules(String repeatModules) {
+        this.repeatModules = repeatModules;
+    }
+
+    public String getCompletedModules() {
+        return completedModules;
+    }
+
+    public void setCompletedModules(String completedModules) {
+        this.completedModules = completedModules;
+    }
+
+    public String getProgramme() {
+        return programme;
+    }
+
+    public void setProgramme(String programme) {
+        this.programme = programme;
+    }
 }
