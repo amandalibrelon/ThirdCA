fd775c25a7bd78caaf13751b46e17908d2272226
amandalibrelon <152657405+amandalibrelon@users.noreply.github.com>
2024-03-31 14:35:24 +0100

    changes to course class for code to run smoothly
    - I had the same problem with the output of courses report, it was not printing what I wanted
    - I add the toString() method override to Courses class .

--- src/thirdca/Courses.java
+++ src/thirdca/Courses.java
@@ -12,26 +12,29 @@ import java.util.List;
  * @author amand
  */
 public class Courses { // class to represent courses
-    
+
     private String courseName;
     private String program;
     private String roomAssignment;
     private List<Students> enrolledStudents;
     private Lecturers lecturer;
-    
-    
-    // constructor
-    public Courses(String courseName, String program, String roomAssignment, Lecturers lecturer){
-       this.courseName = courseName;
-       this.program = program; 
-       this.roomAssignment = roomAssignment; 
-       this.lecturer = lecturer;
-       this.enrolledStudents = new ArrayList<>();
-       
-    }
-    // Getters and setters
- 
 
+    // constructor
+    public Courses(String courseName, String program, String roomAssignment1, Lecturers lecturer1) {
+        this.courseName = courseName;
+        this.program = program;
+        this.roomAssignment = roomAssignment;
+        this.lecturer = lecturer;
+        this.enrolledStudents = new ArrayList<>();
+
+    }
+
+    @Override
+    public String toString() {
+        return "Course Name: " + courseName + ", Program: " + program + ", Room Assignment: " + roomAssignment;
+    }
+
+    // Getters and setters
     public String getCourseName() {
         return courseName;
     }
@@ -72,4 +75,3 @@ public class Courses { // class to represent courses
         this.lecturer = lecturer;
     }
 }
-    
